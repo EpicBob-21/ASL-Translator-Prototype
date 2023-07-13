adaf7294a5a691679e78afbb1aeb24a34bb00866
EpicBob-21 <epicpiebobanana21kimberly@gmail.com>
2023-07-13 13:41:56 -0500

    start new stuff

--- HandTracking.py
+++ HandTracking.py
@@ -3,22 +3,11 @@ import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(0)
-pTime=0
-while True:
-    success, img = cap.read()
-    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    cTime = time.time()
-    fps = 1/(cTime-pTime)
-    pTime=cTime
-    cv2.putText(img, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    cv2.imshow("Test", img)
-    cv2.waitKey(1)
-
-'''
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+'''
 while True:
     success, image = cap.read()
     imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
