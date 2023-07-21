e1e270a828436c3adeafdebf4c2a8f8ed3d6b7ab
EpicBob-21 <epicpiebobanana21kimberly@gmail.com>
2023-07-21 14:37:45 -0500

    cvzone package

--- Translate.py
+++ Translate.py
@@ -1,6 +1,24 @@
 import cv2
 import mediapipe as mp
-import time
-import HandTrackingModule
+from cvzone.HandTrackingModule import HandDetector
 
-HandTrackingModule.main()
+cap=cv2.VideoCapture(0)
+detector = HandDetector(detectionCon=0.8, maxHands=2)
+
+while True:
+    success, img=cap.read()
+    hands, img=detector.findHands(img)
+    #no draw -> hands = =detector.findHands(img, draw=False)
+
+    if hands:
+        #hand 1
+        hand1 = hands[0]
+        lmList1=hand1["lmList"] #list of landmark points
+        bbox1=hand1["bbox"] #bounding box info x,y,w,h
+        centerPoint1=hand1["center"] #center of hand cx, cy
+        handType1=hand1["type"] #left or right
+        fingers1=detector.fingersUp(hand1)
+        print(fingers1)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
