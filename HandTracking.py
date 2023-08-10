711730f8a2d0bf6c361b353f861d97f26cd0e251
EpicBob-21 <epicpiebobanana21kimberly@gmail.com>
2023-08-10 11:12:05 -0500

    data

--- HandTracking.py
+++ HandTracking.py
@@ -2,23 +2,27 @@ import cv2
 import mediapipe as mp
 from cvzone.HandTrackingModule import HandDetector
 
-cap=cv2.VideoCapture(0)
+#cap=cv2.VideoCapture(0)
+
 detector = HandDetector(detectionCon=0.8, maxHands=2)
 
-while True:
-    success, img=cap.read()
-    hands, img=detector.findHands(img)
-    #no draw -> hands = =detector.findHands(img, draw=False)
+#while True:
+img=cv2.imread("test4.jpg")
+img=cv2.resize(img, (400,400))
+#flags=cv2.IMREAD_COLOR
+hands, img=detector.findHands(img)
+#no draw -> hands = =detector.findHands(img, draw=False)
 
-    if hands:
-        #hand 1
-        hand1 = hands[0]
-        lmList1=hand1["lmList"] #list of landmark points
-        bbox1=hand1["bbox"] #bounding box info x,y,w,h
-        centerPoint1=hand1["center"] #center of hand cx, cy
-        handType1=hand1["type"] #left or right
-        fingers1=detector.fingersUp(hand1)
-        print(lmList1)
+if hands:
+    #hand 1
+    hand1 = hands[0]
+    lmList1=hand1["lmList"] #list of landmark points
+    bbox1=hand1["bbox"] #bounding box info x,y,w,h
+    centerPoint1=hand1["center"] #center of hand cx, cy
+    handType1=hand1["type"] #left or right
+    fingers1=detector.fingersUp(hand1)
+    print(lmList1)
+    print(bbox1)
 
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+cv2.imshow("Image", img)
+cv2.waitKey(0)
